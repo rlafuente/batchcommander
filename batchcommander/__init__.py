8abafc92ea1391ec1eb67155cbee559d2fd11dd0
Ricardo Lafuente <r@sollec.org>
2009-06-11 16:57:54 +0530

    a little change to dataset behaviour

--- batchcommander/__init__.py
+++ batchcommander/__init__.py
@@ -48,7 +48,7 @@ class DataSet:
         self.sections = generate_fields(datadict)
         self.widget = None
         self.active = False
-        
+
     def __str__(self):
         return self.name
 
@@ -57,20 +57,21 @@ for result in os.walk(DEFAULT_DATAFILES_DIR):
     for filename in files:
         if os.path.splitext(filename)[1] == '.data':
             filepath = os.path.join(directory, filename)
-            dataset = DataSet(filepath)
-            DEFAULT_DATAFILES.append(dataset)
+            DEFAULT_DATAFILES.append(filepath)
 
 class BatchCommander:
     '''Batch Commander UI runner'''
-    def __init__(self, 
+    def __init__(self,
                  datafiles=DEFAULT_DATAFILES,
-                 inputfile=DEFAULT_INPUTFILE, 
-                 scriptfile=DEFAULT_SCRIPTFILE, 
+                 inputfile=DEFAULT_INPUTFILE,
+                 scriptfile=DEFAULT_SCRIPTFILE,
                  outputfile=DEFAULT_OUTPUTFILE,
                  command=DEFAULT_COMMAND,
                  immediate_mode=DEFAULT_IMMEDIATE_MODE,
                  output_mode=MODE_TEX):
-        self.datasets = datafiles
+        self.datasets = []
+        for path in datafiles:
+            self.datasets.append(DataSet(path))
         self.inputfile = inputfile
         self.scriptfile = scriptfile
         self.outputfile = outputfile
@@ -84,23 +85,23 @@ class BatchCommander:
         self.output_log_filename = 'output.log'
         self.process.setStandardErrorFile(self.error_log_filename)
         self.process.setStandardOutputFile(self.output_log_filename)
-        
+
         # we need this here for now, otherwise it borks
         # this will refer to the combobox in the control window
         self.dataset_selector = None
-        
-    def show_ui(self):       
+
+    def show_ui(self):
         self.app = QtGui.QApplication(sys.argv)
         self.show_main_window()
         self.show_controls_window()
-        
+
     def show_main_window(self):
         '''Create and display the main interface window.'''
         self.main_window = QtGui.QMainWindow()
         self.tab_bar = QtGui.QTabWidget(self.main_window)
         self.status = self.main_window.statusBar()
         main_frame = QtGui.QFrame()
-        
+
         self.infile_textbox = QtGui.QLineEdit(main_frame)
         self.infile_textbox.setGeometry(100, 14, 200, 24)
         self.infile_textbox.setText(self.inputfile)
@@ -108,22 +109,22 @@ class BatchCommander:
         infile_textlabel.setGeometry(10, 14, 50, 24)
         infile_button = QtGui.QPushButton('...', main_frame)
         infile_button.setGeometry(310, 14, 30, 24)
-        main_frame.connect(self.infile_textbox, 
-                           QtCore.SIGNAL('editingFinished()'), 
+        main_frame.connect(self.infile_textbox,
+                           QtCore.SIGNAL('editingFinished()'),
                            self.set_input_file)
         main_frame.connect(infile_button,
                            QtCore.SIGNAL('clicked()'),
                            self.open_infile_dialog)
-                           
+
         self.scriptfile_textbox = QtGui.QLineEdit(main_frame)
         self.scriptfile_textbox.setGeometry(100, 40, 200, 24)
         self.scriptfile_textbox.setText(self.scriptfile)
         scriptfile_textlabel = QtGui.QLabel('Script file', main_frame)
         scriptfile_textlabel.setGeometry(10, 40, 50, 24)
         scriptfile_button = QtGui.QPushButton('...', main_frame)
-        scriptfile_button.setGeometry(310, 40, 30, 24)        
-        main_frame.connect(self.scriptfile_textbox, 
-                           QtCore.SIGNAL('editingFinished()'), 
+        scriptfile_button.setGeometry(310, 40, 30, 24)
+        main_frame.connect(self.scriptfile_textbox,
+                           QtCore.SIGNAL('editingFinished()'),
                            self.set_script_file)
         main_frame.connect(scriptfile_button,
                            QtCore.SIGNAL('clicked()'),
@@ -136,29 +137,39 @@ class BatchCommander:
         outfile_textlabel.setGeometry(10, 66, 50, 24)
         outfile_button = QtGui.QPushButton('...', main_frame)
         outfile_button.setGeometry(310, 66, 30, 24)
-        main_frame.connect(self.outfile_textbox, 
-                           QtCore.SIGNAL('editingFinished()'), 
+        main_frame.connect(self.outfile_textbox,
+                           QtCore.SIGNAL('editingFinished()'),
                            self.set_output_file)
         main_frame.connect(outfile_button,
                            QtCore.SIGNAL('clicked()'),
                            self.open_outfile_dialog)
-        
+
         self.run_button = QtGui.QPushButton('&Run', main_frame)
         self.run_button.setGeometry(190, 100, 100, 30)
-        main_frame.connect(self.run_button, 
-                           QtCore.SIGNAL('clicked()'), 
+        main_frame.connect(self.run_button,
+                           QtCore.SIGNAL('clicked()'),
                            self.run)
         immediate_box = QtGui.QCheckBox('&Immediate mode', main_frame)
         immediate_box.setGeometry(10, 100, 100, 30)
         immediate_box.setChecked(self.immediate_mode)
-        main_frame.connect(immediate_box, 
-                           QtCore.SIGNAL('stateChanged(int)'), 
-                           self.set_immediate_mode)                   
+        main_frame.connect(immediate_box,
+                           QtCore.SIGNAL('stateChanged(int)'),
+                           self.set_immediate_mode)
         self.tab_bar.addTab(main_frame, '&Main')
-        
-        data_frame = QtGui.QFrame()        
+
+        data_frame = QtGui.QFrame()
         list_container = QtGui.QScrollArea(data_frame)
         list_container.setGeometry(10,10, 220, 120)
+
+
+        add_button = QtGui.QPushButton('+', data_frame)
+        add_button.setGeometry(235,10,20,20)
+        remove_button = QtGui.QPushButton('-', data_frame)
+        remove_button.setGeometry(235,35,20,20)
+#        self.main_window.connect(add_button,
+#                                 QtCore.SIGNAL('clicked()'),
+#                                 self.)
+
         self.data_list = QtGui.QListWidget(list_container)
         # list widget size is the same as the container
         self.data_list.setGeometry(list_container.contentsRect())
@@ -176,21 +187,20 @@ class BatchCommander:
         self.data_list.connect(self.data_list,
                                QtCore.SIGNAL('itemChanged(QListWidgetItem *)'),
                                self.on_list_item_changed)
-        # check the initial datafile
         # should instead check the first dataset
         for item in self.data_list_items:
             if item.text() == self.datasets[0].name:
                 item.setCheckState(QtCore.Qt.Checked)
-        
+
         self.tab_bar.addTab(data_frame, '&Data files')
         options_frame = QtGui.QFrame()
         self.tab_bar.addTab(options_frame, '&Options')
-        
+
         tab_bar_height = MAINBOXHEIGHT - self.status.height()
         self.tab_bar.setGeometry(0, 0, MAINBOXWIDTH, tab_bar_height)
         self.main_window.setGeometry(0, 0, MAINBOXWIDTH, MAINBOXHEIGHT)
-        self.main_window.connect(self.process, 
-                                 QtCore.SIGNAL('finished(int)'), 
+        self.main_window.connect(self.process,
+                                 QtCore.SIGNAL('finished(int)'),
                                  self.on_process_finished)
         self.status.showMessage('Ready.')
         self.main_window.show()
@@ -199,54 +209,54 @@ class BatchCommander:
         if self.dataset_selector:
             self.dataset_selector.clear()
             active_datasets = [dataset.name for dataset in self.datasets \
-                                    if dataset.active]    
+                                    if dataset.active]
             self.dataset_selector.addItems(active_datasets)
 
     def show_controls_window(self):
         '''Create and display the controls window.'''
-        
-        self.controls_window = QtGui.QFrame()   
-          
+
+        self.controls_window = QtGui.QFrame()
+
         self.dataset_selector = QtGui.QComboBox(self.controls_window)
         ## self.dataset_selector.setGeometry(5, 5, 100, 30)
         self.update_selector()
         self.controls_window.connect(self.dataset_selector,
                                      QtCore.SIGNAL('activated(int)'),
                                      self.on_selector_changed)
-                  
+
         self.current_dataset = self.datasets[0]
         self.toolbox = QtGui.QToolBox(self.controls_window)
-        
+
         self.update_toolbox()
-        
+
         self.set_immediate_mode(self.immediate_mode)
         self.controls_window.show()
         self.controls_window.setGeometry(0,MAINBOXHEIGHT,FIELDWIDTH+25,400)
         self.toolbox.setGeometry(0,24,FIELDWIDTH+25,400)
-        
+
         sys.exit(self.app.exec_())
-        
+
     def update_toolbox(self):
         # clear it
         for i in range(self.toolbox.count()):
             self.toolbox.removeItem(0)
-        
+
         self.controls = []
         for section in self.current_dataset.sections:
             # count fields
             numberOfFields = len(section.fields)
-            # make frame        
+            # make frame
             container = QtGui.QFrame()
             container.setGeometry(0,0,FIELDWIDTH, FIELDHEIGHT*numberOfFields)
             fieldCount = 0
             for field in section.fields:
-                control = create_control_from_field(field, parent=container, 
-                                                    width=FIELDWIDTH, 
+                control = create_control_from_field(field, parent=container,
+                                                    width=FIELDWIDTH,
                                                     height=FIELDHEIGHT)
                 # place the control in absolute coords -- sucks but works
-                control.setGeometry(0,fieldCount*FIELDHEIGHT, 
+                control.setGeometry(0,fieldCount*FIELDHEIGHT,
                                     FIELDWIDTH, FIELDHEIGHT)
-                self.controls.append(control)         
+                self.controls.append(control)
                 fieldCount += 1
             scrollbox = QtGui.QScrollArea()
             scrollbox.setWidget(container)
@@ -259,28 +269,28 @@ class BatchCommander:
         self.toolbox.setDisabled(True)
         self.status.showMessage('Generating %s...' % (self.scriptfile))
         scriptfile = open(self.scriptfile, 'w')
-    
+
         if self.outputmode == MODE_TEX:
             scriptfile.write('\AtBeginDocument{\n')
             for dataset in self.datasets:
-                if dataset.active:    
+                if dataset.active:
                     for section in dataset.sections:
                         section.output_texstyle(scriptfile)
             scriptfile.write('                }\n')
-            
+
         elif self.outputmode == MODE_PYTHON:
             for dataset in self.datasets:
                 if dataset.active:
                     for section in dataset.sections:
                         section.output_pythonvar(scriptfile)
-                
+
         scriptfile.close()
         self.status.showMessage('Outputting %s...' % (self.outputfile))
-        self.process.start(self.command % {'input_file': self.inputfile, 
+        self.process.start(self.command % {'input_file': self.inputfile,
                                            'output_file': self.outputfile})
-                                  
+
     #### Callbacks ####
-    
+
     def set_input_file(self):
         self.inputfile = str(self.infile_textbox.displayText())
         directory = os.path.dirname(self.inputfile)
@@ -288,17 +298,17 @@ class BatchCommander:
         # track where the log file generated by pdflatex will be
         self.logfile = '%s.log' % (os.path.join(directory,
                                    os.path.splitext(filename)[0]))
-        
+
     def set_script_file(self):
         self.scriptfile = self.scriptfile_textbox.displayText()
-        
+
     def set_output_file(self):
         self.outputfile = self.outfile_textbox.displayText()
-        
+
     def open_infile_dialog(self):
         pwd = os.getcwd()
         # TODO: set parent
-        filename = QtGui.QFileDialog.getOpenFileName(self.main_window, 
+        filename = QtGui.QFileDialog.getOpenFileName(self.main_window,
                                      'Open input file',
                                      pwd,
                                      'TeX/LaTeX files (*.tex)')
@@ -312,7 +322,7 @@ class BatchCommander:
         # track where the log file generated by pdflatex will be
         self.logfile = '%s.log' % (os.path.join(directory,
                                    os.path.splitext(filename)[0]))
-    
+
     def open_scriptfile_dialog(self):
         pwd = os.getcwd()
         # TODO: set parent
@@ -323,7 +333,7 @@ class BatchCommander:
             return False
         self.scriptfile = str(filename)
         self.scriptfile_textbox.setText(self.scriptfile)
-    
+
     def open_outfile_dialog(self):
         pwd = os.getcwd()
         # TODO: set parent
@@ -334,20 +344,20 @@ class BatchCommander:
             return False
         self.outputfile = str(filename)
         self.outfile_textbox.setText(self.outputfile)
-    
+
     def set_immediate_mode(self, value):
         self.immediate_mode = bool(value)
         if self.immediate_mode:
             for control in self.controls:
-                control.connect(control, 
-                                QtCore.SIGNAL('controlChanged()'), 
-                                self.run)            
+                control.connect(control,
+                                QtCore.SIGNAL('controlChanged()'),
+                                self.run)
         else:
             for control in self.controls:
-                control.disconnect(control, 
-                                   QtCore.SIGNAL('controlChanged()'), 
+                control.disconnect(control,
+                                   QtCore.SIGNAL('controlChanged()'),
                                    self.run)
-    
+
     def on_list_item_changed(self, item):
         value = bool(item.checkState())
         for dataset in self.datasets:
@@ -356,18 +366,18 @@ class BatchCommander:
                     self.enable_dataset(dataset)
                 else:
                     self.disable_dataset(dataset)
-        
+
     def on_selector_changed(self, index):
         prev_dataset_name = self.current_dataset.name
         new_dataset_name = self.dataset_selector.itemText(index)
-        
+
         if prev_dataset_name != new_dataset_name:
             for dataset in self.datasets:
                 if dataset.name == new_dataset_name:
                     self.current_dataset = dataset
             self.update_toolbox()
-        
-    
+
+
     def on_process_finished(self, value):
         # error codes from QProcess are not to be trusted, so we also
         # check if the log file has stuff in it
@@ -379,12 +389,12 @@ class BatchCommander:
             self.status.showMessage('Done!')
         self.run_button.setEnabled(True)
         self.toolbox.setEnabled(True)
-        
+
     def add_dataset(self, filepath):
-        dataset = filename = os.path.basename(filepath)
+        dataset = DataSet(filepath)
         self.datasets.append(dataset)
         # TODO: refresh controls window
-        
+
     def remove_dataset(self, dataset):
         for ds in self.datasets:
             if ds.name == dataset.name:
@@ -393,14 +403,25 @@ class BatchCommander:
         # TODO: refresh controls window
         # check if it's the last
         # if it is, grey out the remove box
-        
+
     def enable_dataset(self, dataset):
         log.debug('Enabling dataset ' + dataset.name)
         dataset.active = True
         self.update_selector()
-        
+
     def disable_dataset(self, dataset):
-        # TODO: what do we do if it's the last one selected?
+        # do nothing if it's the last active one
+        dataset_count = 0
+        for ds in self.datasets:
+            if ds.active:
+                dataset_count += 1
+        if dataset_count < 2:
+            # keep the item checked
+            for item in self.data_list_items:
+                if item.text() == dataset.name:
+                    item.setCheckState(QtCore.Qt.Checked)
+            return
+        # otherwise, let's disable it
         log.debug('Disabling dataset ' + dataset.name)
         dataset.active = False
         if dataset == self.current_dataset:
@@ -411,8 +432,8 @@ class BatchCommander:
                     self.current_dataset = dataset
                     self.update_toolbox()
             log.debug('Current dataset set to ' + self.current_dataset.name)
-        self.update_selector()            
-        
+        self.update_selector()
+
     def dataset_by_name(self, name):
         for dataset in self.datasets:
             if name == dataset.name:
