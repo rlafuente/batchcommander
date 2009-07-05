a6ecf65176cabf1965fa954bae49bcdbbbed0199
rlafuente <rlafuente@antid>
2009-07-05 21:56:53 +0530

    moved default settings to the console launcher; added try/except clause on run() to catch errors (still needs proper logging)

--- batchcommander/defaults.py
+++ batchcommander/defaults.py
@@ -10,21 +10,6 @@ CHOICE = 'choice'
 MODE_TEX = 'tex'
 MODE_PYTHON = 'python' 
 
-# TODO: windows version
-homedir = os.path.expanduser("~")
-localdir = os.path.join(homedir, '.batchcommander/')
-datadir = os.path.join(localdir, 'datafiles/')
-examplesdir = os.path.join(localdir, 'examples/')
-
-DEFAULT_INPUTFILE = os.path.join(examplesdir, 'sarovar.tex')
-DEFAULT_SCRIPTFILE = os.path.join(examplesdir, 'river_valley.sty')
-DEFAULT_OUTPUTFILE = os.path.join(homedir, 'output.pdf')
-DEFAULT_DATAFILES_DIR = datadir
-DEFAULT_DATAFILES = []
-# FIXME: the pdf output name is not applied, outputfile not considered
-DEFAULT_COMMAND = 'pdflatex -halt-on-error %(input_file)s %(output_file)s'
-DEFAULT_IMMEDIATE_MODE = True
-
 MAINBOXWIDTH = 370
 MAINBOXHEIGHT = 200
 FIELDHEIGHT = 36
